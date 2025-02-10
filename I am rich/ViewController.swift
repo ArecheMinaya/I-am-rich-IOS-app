dc217054e0d86eef77e0837e7a4a1bf92a22e0e0
ArecheMinaya <arecheminayabj@gmail.com>
2025-02-10 07:31:06 -0800

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Brian Javier Areche Minaya on 2/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
